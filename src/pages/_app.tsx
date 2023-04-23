31225f29e82c1e709021471c53b6512fb15cad39
crislerwin <crisler.silva@alunos.fael.edu.br>
2023-04-23 13:57:31 -0300

    feat: add clerk authentication

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,8 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
 import { api } from "~/@/utils/api";
-
+import { ClerkProvider } from "@clerk/nextjs";
 import "~/@/styles/globals.css";
 
 const MyApp: AppType<{ session: Session | null }> = ({
@@ -12,7 +11,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ClerkProvider {...pageProps}>
+        <Component {...pageProps} />
+      </ClerkProvider>
     </SessionProvider>
   );
 };
