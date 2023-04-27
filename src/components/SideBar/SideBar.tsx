af8f13ead81e2d9f4faa950768504387e825d74a
crislerwin <crisler.silva@alunos.fael.edu.br>
2023-04-26 21:16:31 -0300

    feat: sidebar

---
+++ src/components/SideBar/SideBar.tsx
@@ -0,0 +1,316 @@
+import Link from "next/link";
+import React, { useState } from "react";
+import Image from "next/image";
+
+type Props = {
+  children?: React.ReactNode;
+  userName: string | null;
+  avatarUrl: string;
+};
+
+export const SideBar: React.FC<Props> = ({
+  children,
+  avatarUrl,
+  userName = "",
+}) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  return (
+    <>
+      <div className="fixed z-30 flex h-16 w-full items-center justify-center bg-white p-2 px-10 dark:bg-[#0F172A]">
+        <div
+          className={`logo ${
+            isMenuOpen ? "" : "ml-12"
+          } flex  h-full flex-none transform items-center justify-center duration-500 ease-in-out dark:text-white`}
+        >
+          Health Ops
+        </div>
+        <div className="flex h-full grow items-center justify-center"></div>
+        <div className="flex h-full flex-none items-center justify-center text-center">
+          <div className="flex items-center space-x-3 px-3">
+            <div className="flex flex-none justify-center">
+              <div className="flex h-8 w-8 "></div>
+            </div>
+
+            <div className="md:text-md hidden text-sm text-black dark:text-white md:block">
+              {userName}
+            </div>
+            <div className="flex flex-none justify-center">
+              <Image
+                className="rounded-full"
+                alt={`${userName ?? "user"}'s profile picture`}
+                src={avatarUrl}
+                width={30}
+                height={30}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <aside
+        className={`fixed z-50 flex h-screen w-60  ${
+          isMenuOpen ? "translate-x-none" : "-translate-x-48"
+        } transform bg-[#1E293B] transition duration-1000 ease-in-out`}
+      >
+        <div
+          className={`max-toolbar  absolute -right-6 top-2 flex h-12 w-full ${
+            isMenuOpen ? "translate-x-0" : "translate-x-24 scale-x-0"
+          } transform items-center justify-between rounded-full border-4 border-white bg-[#1E293B]  transition duration-300 ease-in dark:border-[#0F172A]`}
+        >
+          <div className="flex items-center space-x-2 pl-4 ">
+            <div>
+              <div
+                onClick={() => console.log("clicked")}
+                className="moon text-white hover:text-blue-500 dark:hover:text-[#38BDF8]"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={3}
+                  stroke="currentColor"
+                  className="h-4 w-4"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+                  />
+                </svg>
+              </div>
+              <div className="sun hidden text-white hover:text-blue-500 dark:hover:text-[#38BDF8]">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="h-4 w-4"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className="text-white hover:text-blue-500 dark:hover:text-[#38BDF8]">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={3}
+                stroke="currentColor"
+                className="h-4 w-4"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+                />
+              </svg>
+            </div>
+          </div>
+          <div className="group flex items-center space-x-3 rounded-full bg-gradient-to-r from-indigo-500 via-purple-500 to-purple-500 py-1  pl-10 pr-2 text-white dark:from-cyan-500 dark:to-blue-500  ">
+            <div className="mr-12 transform duration-300 ease-in-out">
+              HEALTH
+            </div>
+          </div>
+        </div>
+        <div
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          className="absolute -right-6 top-2 flex transform rounded-full border-4 border-white bg-[#1E293B] p-3 text-white transition duration-500 ease-in-out hover:rotate-45 hover:bg-purple-500 dark:border-[#0F172A] dark:hover:bg-blue-500"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={3}
+            stroke="currentColor"
+            className="h-4 w-4"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"
+            />
+          </svg>
+        </div>
+        <div
+          className={`max mt-20 ${
+            isMenuOpen ? "flex" : "hidden"
+          } h-[calc(100vh)] w-full flex-col space-y-2 text-white`}
+        >
+          <div className="flex w-full transform cursor-pointer flex-row items-center space-x-3 rounded-full bg-[#1E293B] p-2 pl-8 text-white duration-300 ease-in-out hover:ml-4 hover:text-purple-500 dark:hover:text-blue-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-4 w-4"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+              />
+            </svg>
+            <div>Home</div>
+          </div>
+          <div className="flex w-full transform cursor-pointer flex-row items-center space-x-3 rounded-full bg-[#1E293B] p-2 pl-8 text-white duration-300 ease-in-out hover:ml-4 hover:text-purple-500 dark:hover:text-blue-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="h-4 w-4"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+              />
+            </svg>
+            <div>Meus Pacientes</div>
+          </div>
+          <div className="flex w-full transform cursor-pointer flex-row items-center space-x-3 rounded-full bg-[#1E293B] p-2 pl-8 text-white duration-300 ease-in-out hover:ml-4 hover:text-purple-500 dark:hover:text-blue-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="h-4 w-4"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z"
+              />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z"
+              />
+            </svg>
+            <div>Relatorios</div>
+          </div>
+        </div>
+        <div
+          className={`mini mt-20 ${
+            isMenuOpen ? "hidden" : "flex"
+          } h-[calc(100vh)] w-full flex-col space-y-2`}
+        >
+          <div className="flex w-full transform justify-end rounded-full bg-[#1E293B] p-3 pr-5 text-white duration-300 ease-in-out hover:ml-4 hover:text-purple-500 dark:hover:text-blue-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-4 w-4"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+              />
+            </svg>
+          </div>
+          <div className="flex w-full transform justify-end rounded-full bg-[#1E293B] p-3 pr-5 text-white duration-300 ease-in-out hover:ml-4 hover:text-purple-500 dark:hover:text-blue-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="h-4 w-4"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+              />
+            </svg>
+          </div>
+          <div className="flex w-full transform justify-end rounded-full bg-[#1E293B] p-3 pr-5 text-white duration-300 ease-in-out hover:ml-4 hover:text-purple-500 dark:hover:text-blue-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="h-4 w-4"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z"
+              />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z"
+              />
+            </svg>
+          </div>
+        </div>
+      </aside>
+      l
+      <div
+        className={`content ${
+          isMenuOpen ? "ml-12 md:ml-60" : "ml-12"
+        } transform px-2 pb-4 pt-20 duration-500 ease-in-out md:px-5`}
+      >
+        <nav
+          className="flex rounded-lg bg-gray-50 px-5  py-3 text-gray-700 dark:bg-[#1E293B]"
+          aria-label="Breadcrumb"
+        >
+          <ol className="inline-flex items-center space-x-1 md:space-x-3">
+            <li className="inline-flex items-center">
+              <Link
+                href="#"
+                className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
+              >
+                <svg
+                  className="mr-2 h-4 w-4"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
+                </svg>
+                Home
+              </Link>
+            </li>
+            <li>
+              <div className="flex items-center">
+                <svg
+                  className="h-6 w-6 text-gray-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                <Link
+                  href="#"
+                  className="ml-1 text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white md:ml-2"
+                >
+                  Templates
+                </Link>
+              </div>
+            </li>
+          </ol>
+        </nav>
+        {children}
+      </div>
+    </>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,27 @@
 import React from "react";
 import type { NextPage } from "next";
-import { SignOutButton, useClerk } from "@clerk/nextjs";
-
+import { useClerk, useUser } from "@clerk/nextjs";
 import { api } from "@/utils/api";
 import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
 import { getAuth } from "@clerk/nextjs/server";
+import { SideBar } from "@/components/SideBar";
 
 const Home: NextPage = () => {
-  const { data: user, error } = api.persons.getLoggedUser.useQuery();
+  const { signOut } = useClerk();
+  const { user } = useUser();
+  api.persons.getLoggedUser.useQuery(undefined, {
+    onError: (err) => {
+      if (err && err.data?.code === "NOT_FOUND") {
+        signOut().catch((err) => console.log(err));
+      }
+    },
+  });
 
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        {user ? (
-          <div className="text-white">
-            <h1 className="text-4xl font-bold">Welcome {user.userName}!</h1>
-            <p className="text-xl">You are signed in.</p>
-
-            <SignOutButton>
-              <button className="btn">Sign out</button>
-            </SignOutButton>
-          </div>
-        ) : (
-          <></>
-        )}
-      </main>
+      {user && (
+        <SideBar userName={user.fullName} avatarUrl={user.profileImageUrl} />
+      )}
     </>
   );
 };
@@ -33,9 +30,7 @@ export default Home;
 
 export const getServerSideProps = (ctx: CreateNextContextOptions) => {
   const { userId } = getAuth(ctx.req);
-  const unauthorized = !userId && ctx.res.statusCode === 404;
-
-  if (unauthorized) {
+  if (!userId) {
     return {
       redirect: {
         destination: "/sign-in",
