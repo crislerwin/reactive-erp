021a79426fb32ff3ad92e484d1442e909aed3b98
crislerwin <crisler.silva@alunos.fael.edu.br>
2023-04-29 05:35:32 -0300

    feat: table translation

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { MantineReactTable } from "mantine-react-table";
 import type { MRT_SortingState, MRT_Virtualizer } from "mantine-react-table";
 import type { MantineReactTableProps } from "mantine-react-table";
+import { MRT_Localization_PT_BR } from "mantine-react-table/locales/pt-BR";
 
 export const Table: React.FC<MantineReactTableProps> = (props) => {
   const rowVirtualizerInstanceRef =
@@ -17,6 +18,8 @@ export const Table: React.FC<MantineReactTableProps> = (props) => {
       onSortingChange={setSorting}
       rowVirtualizerInstanceRef={rowVirtualizerInstanceRef}
       rowVirtualizerProps={{ overscan: 5 }}
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      localization={MRT_Localization_PT_BR}
       columnVirtualizerProps={{ overscan: 2 }}
       {...props}
     />
