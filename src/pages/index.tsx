f9883f485f2b9048751e30d5e758238236b32cc0
crislerwin <crisler.silva@alunos.fael.edu.br>
2023-05-13 15:24:20 -0300

    fix: adjust to merge

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,47 +2,10 @@ import { getAuth } from "@clerk/nextjs/server";
 import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
 import { type PageType, SideBar, type TabType } from "@/components/SideBar";
 import { useUser } from "@clerk/nextjs";
-import { CustomDataGrid } from "@/components/CustomDataGrid";
-import { faker } from "@faker-js/faker";
-import { IconHome } from "@tabler/icons-react";
 
-const pages: PageType[] = [
-  {
-    page: (
-      <CustomDataGrid
-        columns={[
-          { key: "id", name: "ID", headerCellClass: "rdg-headerCell" },
-          { key: "title", name: "Title", headerCellClass: "rdg-headerCell" },
-          {
-            key: "firstName",
-            name: "First Name",
-            headerCellClass: "rdg-headerCell",
-          },
-          {
-            key: "lastName",
-            name: "Last Name",
-            headerCellClass: "rdg-headerCell",
-          },
-        ]}
-        rows={Array.from({ length: 1000 }, () => ({
-          id: faker.datatype.uuid(),
-          title: faker.name.prefix(),
-          firstName: faker.name.firstName(),
-          lastName: faker.name.lastName(),
-        }))}
-      />
-    ),
-    value: "/",
-  },
-];
+const pages: PageType[] = [];
 
-const tabs: TabType[] = [
-  {
-    href: "/",
-    icon: <IconHome className="h-4 w-4" />,
-    label: "Home",
-  },
-];
+const tabs: TabType[] = [];
 
 const Home = () => {
   const { user } = useUser();
