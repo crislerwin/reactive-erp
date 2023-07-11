fc119b4153a7423b86f9ea0e425f4965c84045e3
crislerwin <crislerwintler@gmail.com>
2023-07-11 17:00:23 -0300

    fix: remove table

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,46 +2,9 @@ import { getAuth } from "@clerk/nextjs/server";
 import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
 import { SideBar } from "@/components/SideBar";
 import { useUser } from "@clerk/nextjs";
-import {
-  CustomDataGrid,
-  headerCellClass,
-  type CustomDataGridProps,
-} from "@/components/CustomDataGrid";
-import { faker } from "@faker-js/faker";
 
 const Table = () => {
-  const rows: CustomDataGridProps["rows"] = Array.from(
-    { length: 1000 },
-    () => ({
-      id: faker.datatype.uuid(),
-      title: faker.name.prefix(),
-      firstName: faker.name.firstName(),
-      lastName: faker.name.lastName(),
-    })
-  );
-  const columns: CustomDataGridProps["columns"] = [
-    {
-      key: "id",
-      name: "ID",
-      headerCellClass: headerCellClass(),
-    },
-    {
-      key: "title",
-      name: "Title",
-      headerCellClass: headerCellClass(),
-    },
-    {
-      key: "firstName",
-      name: "First Name",
-      headerCellClass: headerCellClass(),
-    },
-    {
-      key: "lastName",
-      name: "Last Name",
-      headerCellClass: headerCellClass(),
-    },
-  ];
-  return <CustomDataGrid columns={columns} rows={rows} />;
+  return <></>;
 };
 
 const Home = () => {
