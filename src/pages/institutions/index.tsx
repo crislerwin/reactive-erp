133d06ad74e4f0fc4c708bc44f6344df7bf111ed
crislerwin <crislerwintler@gmail.com>
2024-01-18 23:05:56 -0300

    refactor: remove unecessary code to reestruture the project

--- src/pages/institutions/index.tsx
+++ src/pages/institutions/index.tsx
@@ -1,47 +1,52 @@
 import { SideBar } from "@/components/SideBar";
-import { CompanyForm } from "@/components/pages/institution/Forms";
-import { CompanyTable } from "@/components/pages/institution/Table";
 import { getAuth } from "@clerk/nextjs/server";
-import { Button, Modal } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
-import { IconUserPlus } from "@tabler/icons-react";
 import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
-
+import { useMemo } from "react";
+import { Table } from "@/components/Table";
+import { type MRT_ColumnDef } from "mantine-react-table";
+import { trpc } from "@/utils/api";
 const Companies = () => {
-  const [opened, { open, close }] = useDisclosure(false);
+  const { data: tableData, isFetching } = trpc.institution.findAll.useQuery(
+    undefined,
+    {
+      refetchOnWindowFocus: false,
+    }
+  );
+  const columns = useMemo(
+    (): MRT_ColumnDef<Record<string, unknown>>[] => [
+      {
+        accessorKey: "name",
+        header: "Nome da Instituição",
+        size: 150,
+      },
+      {
+        accessorKey: "company_code",
+        header: "CNPJ",
+        size: 150,
+      },
+      {
+        accessorKey: "email",
+        header: "Email",
+        size: 150,
+      },
 
+      {
+        accessorKey: "additional_info.description",
+        header: "Descrição",
+        size: 150,
+      },
+    ],
+    []
+  );
   return (
     <SideBar>
       <div className="flex flex-col">
-        <div className="mt-4 flex justify-end">
-          <Modal
-            opened={opened}
-            transitionProps={{
-              transition: "fade",
-              duration: 600,
-              timingFunction: "linear",
-            }}
-            onClose={close}
-            centered
-            classNames={{
-              header: "dark:bg-gray-800 dark:text-gray-200 bg-slate-200",
-              title: "dark:text-gray-200 text-gray-600 font-bold",
-              content: "dark:bg-gray-800 dark:text-gray-200 bg-slate-200",
-            }}
-            title="Adicionar Empresa"
-          >
-            <CompanyForm close={close} />
-          </Modal>
-          <Button
-            onClick={open}
-            rightIcon={<IconUserPlus className="h-4 w-4" />}
-            className="add-button bg-slate-200 text-gray-600 hover:bg-slate-100 dark:bg-gray-700 dark:text-gray-200 "
-          >
-            Adicionar Empresa
-          </Button>
-        </div>
         <div className="mt-4 rounded-sm">
-          <CompanyTable />
+          <Table
+            isLoading={isFetching}
+            columns={columns}
+            data={tableData ?? []}
+          />
         </div>
       </div>
     </SideBar>
