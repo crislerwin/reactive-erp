06666d223783e0f1b6429f312c375bab84bd9716
crislerwin <crisler.silva@alunos.fael.edu.br>
2024-07-06 21:07:26 -0300

    chore: allow create staff member

--- src/server/api/__tests__/staff-router.test.ts
+++ src/server/api/__tests__/staff-router.test.ts
@@ -26,4 +26,100 @@ describe("Staff Router", () => {
     const promise = sut.staff.findAll({ branch_id });
     await expect(promise).rejects.toThrowError("Branch not found");
   });
+
+  test("Should return staff list", async () => {
+    const sut = makeSut();
+    const branch = await prisma.branch.create({
+      data: {
+        name: faker.company.name(),
+        company_code: faker.helpers.fake("###-###-###"),
+        email: faker.internet.email(),
+      },
+    });
+    await prisma.staff.createMany({
+      data: [
+        {
+          branch_id: branch.branch_id,
+          account_id: faker.datatype.number(),
+          first_name: faker.name.firstName(),
+          role: "ADMIN",
+        },
+        {
+          branch_id: branch.branch_id,
+          account_id: faker.datatype.number(),
+          first_name: faker.name.firstName(),
+          role: "ADMIN",
+        },
+      ],
+    });
+    const result = await sut.staff.findAll({ branch_id: branch.branch_id });
+    expect(result).toBeDefined();
+    expect(result).toBeInstanceOf(Array);
+    expect(result.length).toBe(2);
+  });
+
+  describe("Create Staff Member", () => {
+    test("Should throw if branch not found", async () => {
+      const sut = makeSut();
+      const branch_id = faker.datatype.number();
+      const promise = sut.staff.createStaffMember({
+        branch_id,
+        first_name: faker.name.firstName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        role: "ADMIN",
+      });
+      await expect(promise).rejects.toThrowError("Branch not found");
+    });
+
+    test("Should throw if account already exists", async () => {
+      const sut = makeSut();
+      const branch = await prisma.branch.create({
+        data: {
+          name: faker.company.name(),
+          company_code: faker.helpers.fake("###-###-###"),
+          email: faker.internet.email(),
+        },
+      });
+      const account = await prisma.account.create({
+        data: {
+          email: faker.internet.email(),
+          name: faker.name.firstName(),
+          password: faker.internet.password(),
+        },
+      });
+      const promise = sut.staff.createStaffMember({
+        branch_id: branch.branch_id,
+        first_name: faker.name.firstName(),
+        email: account.email,
+        password: faker.internet.password(),
+        role: "ADMIN",
+      });
+      await expect(promise).rejects.toThrowError("Account already exists");
+    });
+
+    test("Should create staff member", async () => {
+      const sut = makeSut();
+      const branch = await prisma.branch.create({
+        data: {
+          name: faker.company.name(),
+          company_code: faker.helpers.fake("###-###-###"),
+          email: faker.internet.email(),
+        },
+      });
+      const result = await sut.staff.createStaffMember({
+        branch_id: branch.branch_id,
+        first_name: faker.name.firstName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        role: "ADMIN",
+      });
+      expect(result).toBeDefined();
+      expect(result).toHaveProperty("id");
+      expect(result).toHaveProperty("account_id");
+      expect(result).toHaveProperty("branch_id");
+      expect(result).toHaveProperty("role");
+      expect(result).toHaveProperty("first_name");
+    });
+  });
 });
--- src/server/api/routers/staff.router.ts
+++ src/server/api/routers/staff.router.ts
@@ -14,4 +14,48 @@ export const staffRouter = createTRPCRouter({
 
       return ctx.prisma.staff.findMany({ where: input });
     }),
+  createStaffMember: protectedProcedure
+    .input(
+      z.object({
+        branch_id: z.number(),
+        first_name: z.string(),
+        last_name: z.string().optional(),
+        email: z.string().email(),
+        password: z.string(),
+        role: z.enum(["ADMIN", "EMPLOYEE", "MANAGER", "OWNER"]),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const branch = await ctx.prisma.branch.findUnique({
+        where: { branch_id: input.branch_id },
+      });
+
+      if (!branch)
+        throw new TRPCError({ code: "NOT_FOUND", cause: "Branch not found" });
+      const existingAccount = await ctx.prisma.account.findUnique({
+        where: { email: input.email },
+      });
+      if (existingAccount)
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          cause: "Account already exists",
+        });
+
+      const newAccount = await ctx.prisma.account.create({
+        data: {
+          email: input.email,
+          name: input.first_name,
+          last_name: input.last_name,
+          password: input.password,
+        },
+      });
+      return ctx.prisma.staff.create({
+        data: {
+          account_id: newAccount.account_id,
+          branch_id: input.branch_id,
+          role: input.role,
+          first_name: input.first_name,
+        },
+      });
+    }),
 });
