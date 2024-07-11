6f91b0fa6e393ee03e19d99d356a5d1b166b92c7
crislerwin <crisler.wintler@splitc.com.br>
2024-07-10 22:15:49 -0300

    chore: branch crud

--- src/server/api/__tests__/branch-router.test.ts
+++ src/server/api/__tests__/branch-router.test.ts
@@ -33,4 +33,99 @@ describe("Branch router", () => {
       );
     });
   });
+  describe("CREATE branch", () => {
+    it.only("should create a new branch", async () => {
+      const { app } = await makeSut();
+      const branch = await app.branch.createBranch({
+        name: faker.company.name(),
+        company_code: faker.random.alphaNumeric(10),
+        email: faker.internet.email(),
+        logo_url: faker.image.imageUrl(),
+        attributes: {
+          test: "test",
+        },
+      });
+      expect(branch).toHaveProperty("branch_id");
+      expect(branch).toHaveProperty("name");
+      expect(branch).toHaveProperty("company_code");
+      expect(branch).toHaveProperty("email");
+      expect(branch).toHaveProperty("logo_url");
+      expect(branch).toHaveProperty("created_at");
+      expect(branch).toHaveProperty("updated_at");
+      expect(branch).toHaveProperty("deleted_at");
+      expect(branch).toHaveProperty("attributes");
+    });
+    it("should return an error if the user is not allowed to perform this action", async () => {
+      const app = makeApp({
+        branch_id: 1,
+        staff: {
+          active: true,
+          id: 1,
+          branch_id: 1,
+          created_at: new Date(),
+          updated_at: new Date(),
+          deleted_at: null,
+          email: faker.internet.email(),
+          first_name: faker.name.firstName(),
+          last_name: faker.name.lastName(),
+          role: "EMPLOYEE",
+        },
+      });
+      const promisses = app.branch.createBranch({
+        name: faker.company.name(),
+        company_code: faker.random.alphaNumeric(10),
+        email: faker.internet.email(),
+        logo_url: faker.image.imageUrl(),
+        attributes: {
+          test: "test",
+        },
+      });
+      await expect(promisses).rejects.toThrowError(
+        "You are not allowed to perform this action"
+      );
+    });
+  });
+  describe("DELETE branch", () => {
+    it("should delete a branch", async () => {
+      const { app } = await makeSut();
+      const branch = await app.branch.createBranch({
+        name: faker.company.name(),
+        company_code: faker.random.alphaNumeric(10),
+        email: faker.internet.email(),
+        logo_url: faker.image.imageUrl(),
+        attributes: {
+          test: "test",
+        },
+      });
+      const deletedBranch = await app.branch.deleteBranch({
+        branch_id: branch.branch_id,
+      });
+      expect(deletedBranch).toHaveProperty("branch_id");
+      expect(deletedBranch).toHaveProperty("deleted_at");
+      expect(deletedBranch.deleted_at).not.toBeNull();
+    });
+    it("should return an error if the user is not allowed to perform this action", async () => {
+      const app = makeApp({
+        branch_id: 1,
+        staff: {
+          active: true,
+          id: 1,
+          branch_id: 1,
+          created_at: new Date(),
+          updated_at: new Date(),
+          deleted_at: null,
+          email: faker.internet.email(),
+          first_name: faker.name.firstName(),
+          last_name: faker.name.lastName(),
+          role: "EMPLOYEE",
+        },
+      });
+      const promisses = app.branch.deleteBranch({
+        branch_id: 1,
+      });
+      await expect(promisses).rejects.toThrowError(
+        "You are not allowed to perform this action"
+      );
+    });
+  });
 });
--- src/server/api/routers/branch/branch.router.ts
+++ src/server/api/routers/branch/branch.router.ts
@@ -1,17 +1,54 @@
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
 import { TRPCError } from "@trpc/server";
+import { z } from "zod";
 
+export const createBranchSchema = z.object({
+  name: z.string(),
+  logo_url: z.string().optional(),
+  email: z.string(),
+  company_code: z.string(),
+  attributes: z.record(z.string()).optional(),
+});
+const validateRole = (role: string) => {
+  if (!allowedRoles.includes(role))
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      cause: "You are not allowed to perform this action",
+    });
+};
 const allowedRoles = ["ADMIN", "MANAGER", "OWNER"];
 export const branchRouter = createTRPCRouter({
   findAll: protectedProcedure
     .meta({ method: "GET", path: "/branch" })
     .query(async ({ ctx }) => {
-      if (!allowedRoles.includes(ctx.session.account.role))
-        throw new TRPCError({
-          code: "UNAUTHORIZED",
-          cause: "You are not allowed to perform this action",
-        });
-
-      return ctx.prisma.branch.findMany();
+      validateRole(ctx.session.account.role);
+      return ctx.prisma.branch.findMany({
+        where: { deleted_at: null },
+      });
+    }),
+  createBranch: protectedProcedure
+    .meta({ method: "POST", path: "/branch" })
+    .input(createBranchSchema)
+    .mutation(async ({ ctx, input }) => {
+      validateRole(ctx.session.account.role);
+      return ctx.prisma.branch.create({
+        data: {
+          name: input.name,
+          logo_url: input.logo_url,
+          company_code: input.company_code,
+          email: input.email,
+          attributes: input.attributes,
+        },
+      });
+    }),
+  deleteBranch: protectedProcedure
+    .meta({ method: "DELETE", path: "/branch" })
+    .input(z.object({ branch_id: z.number() }))
+    .mutation(async ({ ctx, input }) => {
+      validateRole(ctx.session.account.role);
+      return ctx.prisma.branch.update({
+        where: { branch_id: input.branch_id },
+        data: { deleted_at: new Date() },
+      });
     }),
 });
